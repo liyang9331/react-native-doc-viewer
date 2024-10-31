20e526e8d829d08ae6929f835679bba6a8019ce1
LiYang <70904270+liyang9331@users.noreply.github.com>
2024-10-31 17:27:16 +0800

    Update RNDocViewerModule.java

--- android/src/main/java/com/philipphecht/RNDocViewerModule.java
+++ android/src/main/java/com/philipphecht/RNDocViewerModule.java
@@ -9,7 +9,10 @@ import com.facebook.react.bridge.WritableNativeMap;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.ReadableArray;
-import com.facebook.react.views.webview.ReactWebViewManager;
+// import com.facebook.react.views.webview.ReactWebViewManager;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.views.webview.ReactWebView;
+
 
 /* bridge react native
 int size();
@@ -33,7 +36,6 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 // AndroidX 兼容性更新
 androidx.core.content.FileProvider
-// import android.support.v4.content.FileProvider;
 
 import android.content.ActivityNotFoundException;
 import android.content.Context;
