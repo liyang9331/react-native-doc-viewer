627ef735cef86271a3117660f91fe32ad7bd1712
LiYang <70904270+liyang9331@users.noreply.github.com>
2024-10-31 11:07:41 +0800

    Update RNDocViewerModule.java

--- android/src/main/java/com/philipphecht/RNDocViewerModule.java
+++ android/src/main/java/com/philipphecht/RNDocViewerModule.java
@@ -31,10 +31,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-
-// 迁移到AndroidX
-import androidx.core.content.FileProvider;
-// import android.support.v4.content.FileProvider;
+import android.support.v4.content.FileProvider;
 
 import android.content.ActivityNotFoundException;
 import android.content.Context;
