174be5c0351adee9db8c4fa8099313d23bd337e8
LiYang <70904270+liyang9331@users.noreply.github.com>
2024-10-31 15:52:42 +0800

    Update FileProviderClass.java

--- android/src/main/java/com/philipphecht/FileProviderClass.java
+++ android/src/main/java/com/philipphecht/FileProviderClass.java
@@ -1,7 +1,4 @@
 package com.philipphecht;
-
-// public class FileProviderClass extends android.support.v4.content.FileProvider {
-// }
 // AndroidX 兼容性更新
 public class FileProviderClass extends androidx.core.content.FileProvider {
 }
